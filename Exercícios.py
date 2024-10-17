16c2938a9c88ad819f638ed227f61267f5d87588
BRCMOURA <brcmoura@gmail.com>
2024-10-16 21:30:07 -0300

    Add files via upload

---
+++ Exercícios.py
@@ -0,0 +1,90 @@
+# 1)Peça ao usuário um número e informe se ele é positivo, negativo ou zero
+
+#num = int(input("Digite um número:"))
+
+#if num > 0:
+#    print('O número é positivo')
+
+#elif num == 0:
+#    print('O número é igual a zero')
+
+#else:
+#    print('O número é negativo')
+
+# 2)Peça a idade do usuário e informe se ele é maior de idade
+
+#idade = int(input("Digite a sua idade:"))
+
+#if idade >= 18:
+#    print('O usuário é maior de idade')
+
+#else:
+#    print('O usuário é menor de idade')
+
+# 3) Peça um número ao usuário e informe se ele é par ou ímpar
+
+# num = int(input("Digite um número:"))
+
+# if num % 2 == 0:
+#     print(f'O número {num} é par')
+
+# else:
+#     print(f'O número {num} é impar')
+
+# 4) Peça ao usuário dois números e informe qual é o maior
+
+# num1 = int(input("Digite o primeiro número:"))
+# num2 = int(input("Digite o segundo número:"))
+
+# if num1 > num2:
+#     print(f'O número {num1} é maior que {num2}')
+
+# else:
+#     print(f'O número {num2} é maior que {num1}')
+
+# 5) Peça ao usuário o valor de uma compra; se o valor for maior que R$ 100 aplique um desconto de 10%; caso contrário exiba o valor sem desconto
+
+# valor_compra = float(input("Digite o valor da compra:"))
+
+# if valor_compra > 100:
+#     desconto = valor_compra * 0.1
+#     total = valor_compra - desconto
+   
+# else:
+#     total = valor_compra
+
+# print(f'O valor da compra é R$: {total:.2f}')
+
+# 6) Peça ao usuário um número e verifique se ele é múltiplo de 5
+
+# num = int(input("Digite um número:"))
+
+# if num % 5 == 0:
+#     print(f'O número {num} é múltiplo de 5')
+
+# else:
+#     print(f'O número {num} não é múltiplo de 5')
+
+# 7) Peça três notas ao usuário e calcule a média; se a média for maior ou igual a 7 informe que o aluno foi aprovado; senão, reprovado
+
+# nota1 = float(input("Digite a primeira nota:"))
+# nota2 = float(input("Digite a segunda nota:"))
+# nota3 = float(input("Digite a terceira nota:"))
+
+# media = (nota1 + nota2 + nota3) / 3
+
+# if media >= 7:
+#     print(f'O aluno foi aprovado com média {media:.2f}')
+
+# else:
+#     print(f'O aluno foi reprovado com média {media:.2f}')
+
+# 8) Crie um programa que peça uma senha ao usuário; se a senha for "python123_EFG" exiba "Acesso permitido"; caso contrário "Senha incorreta"
+
+# senha = input("Digite a senha:")
+
+# if senha == 'python123_EFG':
+#    print("Acesso permitido")
+
+# else:
+#    print("Senha incorreta")
